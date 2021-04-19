4a67eede0282d1712b2e487d8a9c9a6afbf80f67
Lucas <lucas.jdev1@gmail.com>
2021-04-19 10:10:56 -0300

    implementação de data e hora em tempo real

---
+++ frontend/scripts/realtime.js
@@ -0,0 +1,29 @@
+var data;
+
+function formataData() {
+    data = new Date();
+    var dia = data.getDate();
+    var mes = data.getMonth() + 1;
+
+    dia = ('0' + dia).slice(-2);
+    mes = ('0' + mes).slice(-2);
+
+    document.getElementById('date').innerHTML = dia + '/' + mes;
+}
+
+setInterval(formataHoraMinuto, 1000);
+
+function formataHoraMinuto() {
+    data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var segundo = data.getSeconds();
+
+    hora = ('0' + hora).slice(-2);
+    minuto = ('0' + minuto).slice(-2);
+    segundo = ('0' + segundo).slice(-2);
+
+    console.log(document.getElementById('clock').innerHTML = hora + ':' + minuto + ':' + segundo);
+    formataData()
+
+}
